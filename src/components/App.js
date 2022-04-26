ab719e0a24f33bb03ae70cf7816032b3cb53799b
miranda-zapata <miranda.zapata@gmail.com>
2022-04-26 09:12:40 +0200

    add first task: paint quotes' list

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,25 @@
 import '../styles/App.scss';
+import { useState } from 'react';
+import quoteList from '../data/quotes.json';
 
 function App() {
+  // Para comprobar si quoteList nos trae correctamente los datos. BORRAR
+  console.log(quoteList);
+
+  // Variables de estado
+  const [data, setData] = useState(quoteList);
+
+  const htmlData = data.map((quote, index) => (
+    <li className='quote' key={index}>
+      <p className='quote__item'>{quote.quote}</p>
+      <span className='quote__character'>{quote.character}</span>
+    </li>
+  ));
+
   return (
     <>
       <h1>F·R·I·E·N·D·S' quotes</h1>
+      <ul>{htmlData}</ul>
     </>
   );
 }
