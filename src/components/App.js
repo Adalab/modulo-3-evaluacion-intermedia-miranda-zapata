72e13aa7226a2499167555f84b06a51970cad23c
miranda-zapata <miranda.zapata@gmail.com>
2022-04-26 09:55:08 +0200

    add more code but also gitHub pages

--- src/components/App.js
+++ src/components/App.js
@@ -8,18 +8,94 @@ function App() {
 
   // Variables de estado
   const [data, setData] = useState(quoteList);
+  const [newQuote, setNewQuote] = useState({
+    quote: '',
+    character: '',
+  });
+  const [searchQuote, setSearchQuote] = useState('');
 
-  const htmlData = data.map((quote, index) => (
-    <li className='quote' key={index}>
-      <p className='quote__item'>{quote.quote}</p>
-      <span className='quote__character'>{quote.character}</span>
-    </li>
-  ));
+  const handleNewQuote = (event) => {
+    setNewQuote({
+      ...newQuote,
+      [event.target.id]: event.target.value,
+    });
+  };
+
+  const handleClickAdd = (event) => {
+    event.preventDefault();
+    setData([...data, newQuote]);
+    setNewQuote({
+      quote: '',
+      character: '',
+    });
+  };
+
+  const handleSearchQuote = (event) => {
+    setSearchQuote(event.target.value);
+  };
+
+  const htmlData = data
+    .filter(
+      (quote) =>
+        quote.quote.toLowerCase().includes(searchQuote.toLowerCase()) ||
+        quote.character.toLowerCase().includes(searchQuote.toLowerCase())
+    )
+    .map((quote, index) => {
+      return (
+        <li className='quote' key={index}>
+          <p className='quote__item'>{quote.quote}</p>
+          <span className='quote__character'>{quote.character}</span>
+        </li>
+      );
+    });
 
   return (
     <>
-      <h1>F·R·I·E·N·D·S' quotes</h1>
+      <header className='header'>
+        <h1 className='header__title'>F·R·I·E·N·D·S' quotes</h1>
+        <form>
+          <input
+            className='searchQuote__input'
+            autoComplete='off'
+            type='search'
+            name='searchQuote'
+            placeholder='Search quote'
+            onChange={handleSearchQuote}
+            value={searchQuote}
+          />
+        </form>
+      </header>
+
       <ul>{htmlData}</ul>
+
+      <main className='main'>
+        <form>
+          <input
+            className='addQuote__input'
+            type='text'
+            name='addQuote'
+            id='addQuote'
+            placeholder='Do you know any more quotes?'
+            onChange={handleNewQuote}
+            value={newQuote.quote}
+          />
+          <input
+            className='addCharacter__input'
+            type='text'
+            name='addCharacter'
+            id='addCharacter'
+            placeholder='Who said it?'
+            onChange={handleNewQuote}
+            value={newQuote.character}
+          />
+          <input
+            className='addBtn'
+            type='submit'
+            value='Add new quote'
+            onClick={handleClickAdd}
+          />
+        </form>
+      </main>
     </>
   );
 }
